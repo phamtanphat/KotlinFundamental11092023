77eb779ce5d2e0db752a831f704462266180bee8
phamtanphat <phatdroid94@gmail.com>
2023-09-18 21:19:50 +0700

    List

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,10 +8,27 @@ fun main(args: Array<String>) {
     // Kieu luan ly: Boolean (true, false)
     // Kieu chuoi: String
 
-    val name: String = "Pham Tan Phat"
-    var weight: Double = 70.5
-    var height: Float = 1.75f
-    var age = 29
-    var isMale = true
-    for test
+    // val name: String = "Pham Tan Phat"
+    // var weight: Double = 70.5
+    // var height: Float = 1.75f
+    // var age = 29
+    // var isMale = true
+
+    // 3: Collections
+
+    // List
+    // Immutable list
+    // val listNumbers = listOf<Int>()
+    // Mutable list
+    val listNumbers = mutableListOf<Int>()
+    // Them phan tu
+    listNumbers.add(10) // index = 0
+    listNumbers.add(20) // index = 1
+    listNumbers.add(30) // index = 2
+    // Xoa phan tu
+    listNumbers.removeAt(2)
+    // Cap nhat phan tu
+    listNumbers[0] = 9
+
+    print(listNumbers.size.toString())
 }
