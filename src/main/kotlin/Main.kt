d7fc8fd9c8c0fe08fcd855578b805b54b762d1d7
phamtanphat <phatdroid94@gmail.com>
2023-09-18 20:37:23 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    // 1: Khai bao bien
+    // Immutable (val) va mutable (var)
+
+    // 2: Kieu du lieu
+    // Kieu so nguyen: Int, Long
+    // Kieu so thuc: Float, Double
+    // Kieu luan ly: Boolean (true, false)
+    // Kieu chuoi: String
+
+    val name: String = "Pham Tan Phat"
+    var weight: Double = 70.5
+    var height: Float = 1.75f
+    var age = 29
+    var isMale = true
+}
