c1bcdb7f19aff6f4a41217c53af4fe072728e652
phamtanphat <phatdroid94@gmail.com>
2023-09-25 20:14:17 +0700

    if else gia tri tra ve

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,129 +1,144 @@
 fun main(args: Array<String>) {
-    // 1: Khai bao bien
-    // Immutable (val) va mutable (var)
+//     1: Khai bao bien
+//     Immutable (val) va mutable (var)
+//
+//     2: Kieu du lieu
+//     Kieu so nguyen: Int, Long
+//     Kieu so thuc: Float, Double
+//     Kieu luan ly: Boolean (true, false)
+//     Kieu chuoi: String
+//
+//     val name: String = "Pham Tan Phat"
+//     var weight: Double = 70.5
+//     var height: Float = 1.75f
+//     var age = 29
+//     var isMale = true
+//
+//     3: Collections
+//
+//     List
+//     Immutable list
+//     val listNumbers = listOf<Int>()
+//     Mutable list
+//     val listNumbers = mutableListOf<Int>()
+//     Them phan tu
+//     listNumbers.add(10) // index = 0
+//     listNumbers.add(20) // index = 1
+//     listNumbers.add(30) // index = 2
+//     Xoa phan tu
+//     listNumbers.removeAt(2)
+//     Cap nhat phan tu
+//     listNumbers[0] = 9
+//     print(listNumbers.size.toString())
+//
+//     Set: Khong luu gia tri trung
+//     Khai bao
+//     val setPersons = mutableSetOf<String>()
+//     Them du lieu
+//     setPersons.add("Teo")
+//     setPersons.add("Teo")
+//     print(setPersons.size.toString())
+//
+//     Map
+//     val mapProducts = mutableMapOf(
+//        "name" to "Ti",
+//        "age" to 5,
+//        "address" to "Quan 10"
+//    )
+//
+//    print(mapProducts["name"])
+//    print(mapProducts["age"])
+//
+//     4: Toan tu
+//     var a = 10
+//     a = a + 1
+//     a += 1
+//     var b = a++ // a = 11
+//     var c = ++a // a = 12
+//
+//     a++
+//     Gan gia tri
+//     Tang a them 1 don vi (a += 1)
+//
+//     ++a
+//     Tang a them 1 don vi (a += 1)
+//     Gan gia tri
+//
+//     print("B $b") // B = 10
+//     print("C $c") // C = 12
+//
+//     var a = 3
+//     var b = 5
+//
+//     var result = a++ - b-- + --b - a-- + b++ - --a
+//     3 - b-- + --b - a-- + b++ - --a ; a = 4 , b = 5
+//     3 - 5 + --b - a-- + b++ - --a ; a = 4 , b = 4
+//     3 - 5 + 3 - a-- + b++ - --a ; a = 4 , b = 3
+//     3 - 5 + 3 - 4 + b++ - --a ; a = 3 , b = 3
+//     3 - 5 + 3 - 4 + 3 - --a ; a = 3 , b = 4
+//     3 - 5 + 3 - 4 + 3 - 2 ; a = 2 , b = 4
+//     result = -2, a = 2, b = 4
+//     print("result = $result, a = $a, b = $b")
+//
+//     5: Cau dieu kien
+//     if else
+//
+//     var a = 5
+//     var b = 10
+//
+//     Neu a lon b: In ra a lon b
+//     Neu a nho b: In ra a nho b
+//     Neu a bang b: In ra a bang b
+//
+//     {} : scope (Pham vi)
+//     () : chua bieu thuc
+//
+//     if (a > b) {
+//        print("A lon hon b")
+//     } else if (a == b) {
+//        print("A bang b")
+//     } else {
+//        print("A be hon b")
+//     }
+//
+//     Vi du: Tinh BMI
 
-    // 2: Kieu du lieu
-    // Kieu so nguyen: Int, Long
-    // Kieu so thuc: Float, Double
-    // Kieu luan ly: Boolean (true, false)
-    // Kieu chuoi: String
+//    val weight = 70.5
+//    val height = 1.75
+//    val bmi = weight / (height * height)
 
-    // val name: String = "Pham Tan Phat"
-    // var weight: Double = 70.5
-    // var height: Float = 1.75f
-    // var age = 29
-    // var isMale = true
+//     && => Chi dung khi va chi khi tat ca deu dung
+//     || => Chi sai khi tat ca deu sai
 
-    // 3: Collections
+//    if (bmi < 18.5) {
+//        print("Can nang thap (gay)")
+//    } else if (bmi <= 24.9) {
+//        print("Can nang binh thuong")
+//    } else if (bmi == 25.toDouble()) {
+//        print("Can nang binh thuong")
+//    } else if (bmi <= 29.9) {
+//        print("Tien beo phi")
+//    } else if (bmi <= 34.9) {
+//        print("Tien beo phi do 1")
+//    } else if (bmi <= 39.9) {
+//        print("Tien beo phi do 2")
+//    } else {
+//        Case bmi >= 40
+//        print("Tien beo phi do 3")
+//    }
 
-    // List
-    // Immutable list
-    // val listNumbers = listOf<Int>()
-    // Mutable list
-    // val listNumbers = mutableListOf<Int>()
-    // Them phan tu
-    // listNumbers.add(10) // index = 0
-    // listNumbers.add(20) // index = 1
-    // listNumbers.add(30) // index = 2
-    // Xoa phan tu
-    // listNumbers.removeAt(2)
-    // Cap nhat phan tu
-    // listNumbers[0] = 9
-    // print(listNumbers.size.toString())
-
-    // Set: Khong luu gia tri trung
-    // Khai bao
-    // val setPersons = mutableSetOf<String>()
-    // Them du lieu
-    // setPersons.add("Teo")
-    // setPersons.add("Teo")
-    // print(setPersons.size.toString())
-
-    // Map
-    // val mapProducts = mutableMapOf(
-    //    "name" to "Ti",
-    //    "age" to 5,
-    //    "address" to "Quan 10"
-    //)
-
-    //print(mapProducts["name"])
-    //print(mapProducts["age"])
-
-    // 4: Toan tu
-    // var a = 10
-    // a = a + 1
-    // a += 1
-    // var b = a++ // a = 11
-    // var c = ++a // a = 12
-
-    // a++
-    // Gan gia tri
-    // Tang a them 1 don vi (a += 1)
-
-    // ++a
-    // Tang a them 1 don vi (a += 1)
-    // Gan gia tri
-
-    // print("B $b") // B = 10
-    // print("C $c") // C = 12
-
-    // var a = 3
-    // var b = 5
-
-    // var result = a++ - b-- + --b - a-- + b++ - --a
-    // 3 - b-- + --b - a-- + b++ - --a ; a = 4 , b = 5
-    // 3 - 5 + --b - a-- + b++ - --a ; a = 4 , b = 4
-    // 3 - 5 + 3 - a-- + b++ - --a ; a = 4 , b = 3
-    // 3 - 5 + 3 - 4 + b++ - --a ; a = 3 , b = 3
-    // 3 - 5 + 3 - 4 + 3 - --a ; a = 3 , b = 4
-    // 3 - 5 + 3 - 4 + 3 - 2 ; a = 2 , b = 4
-    // result = -2, a = 2, b = 4
-    // print("result = $result, a = $a, b = $b")
-
-    // 5: Cau dieu kien
-    // if else
-
-    // var a = 5
-    // var b = 10
-
-    // Neu a lon b: In ra a lon b
-    // Neu a nho b: In ra a nho b
-    // Neu a bang b: In ra a bang b
-
-    // {} : scope (Pham vi)
-    // () : chua bieu thuc
-
-    // if (a > b) {
-    //    print("A lon hon b")
-    // } else if (a == b) {
-    //    print("A bang b")
-    // } else {
-    //    print("A be hon b")
-    // }
-
-    // Vi du: Tinh BMI
-
-    val weight = 70.5
-    val height = 1.75
-    val bmi = weight / (height * height)
-
-    // && => Chi dung khi va chi khi tat ca deu dung
-    // || => Chi sai khi tat ca deu sai
-
-    if (bmi < 18.5) {
-        print("Can nang thap (gay)")
-    } else if (bmi <= 24.9) {
-        print("Can nang binh thuong")
-    } else if (bmi == 25.toDouble()) {
-        print("Can nang binh thuong")
-    } else if (bmi <= 29.9) {
-        print("Tien beo phi")
-    } else if (bmi <= 34.9) {
-        print("Tien beo phi do 1")
-    } else if (bmi <= 39.9) {
-        print("Tien beo phi do 2")
+//    Su dung if else co gia tri tra ve
+//    alt + enter: phim goi y sua loi
+//    Khi khai báo các biến có miền giá trị như bên dưới
+//      YES / NO
+//      ON / OFF
+//      TRUE / FALSE
+//   thêm is phía trước tên biến => isChecked
+    var checkBox = true // Example: Dang kiem tra trang thai check cho CheckBox
+    var isChecked = if (checkBox) {
+        print("Hello")
+        true
     } else {
-        // Case bmi >= 40
-        print("Tien beo phi do 3")
+        false
     }
 }
