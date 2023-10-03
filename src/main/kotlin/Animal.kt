8cb94cedd2c213a49256e794c384448065203690
phamtanphat <phatdroid94@gmail.com>
2023-10-03 21:30:21 +0700

    nullable

--- src/main/kotlin/Animal.kt
+++ src/main/kotlin/Animal.kt
@@ -1,4 +1,4 @@
-class Animal constructor(var name: String, var weight: Float, var color: String) {
+class Animal constructor(var name: String?, var weight: Float?, var color: String?) {
     // 1: Thuoc tinh
 //    var name: String = ""
 //    var weight: Float = 0.0f
