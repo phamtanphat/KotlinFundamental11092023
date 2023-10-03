d8bcaccf2f7b266caa55bb09548140b06ea43547
phamtanphat <phatdroid94@gmail.com>
2023-10-03 21:26:22 +0700

    constructor

--- src/main/kotlin/Animal.kt
+++ src/main/kotlin/Animal.kt
@@ -1,2 +1,7 @@
-class Animal {
+class Animal constructor(var name: String, var weight: Float, var color: String) {
+    // 1: Thuoc tinh
+//    var name: String = ""
+//    var weight: Float = 0.0f
+//    var color: String = ""
+    // 2: Phuong thuc
 }
