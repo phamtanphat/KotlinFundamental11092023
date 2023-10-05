32322e08eec44f397c40cb894eb86388fedf42a3
phamtanphat <phatdroid94@gmail.com>
2023-10-05 20:34:12 +0700

    khai bao secondaray constructor co primary constructor

--- src/main/kotlin/Animal.kt
+++ src/main/kotlin/Animal.kt
@@ -1,29 +1,22 @@
-class Animal {
+class Animal (
+    var name: String,
+    var weight: Float,
+    var color: String
+) {
 //     1: Thuoc tinh
 //    var name: String = ""
 //    var weight: Float = 0.0f
 //    var color: String = ""
 //     2: Phuong thuc
 
-//    Secondary constructor
-    var name: String = ""
-    var weight: Float = 0.0f
-    var color: String = ""
     var speed: String = ""
-
-    constructor(name: String, weight: Float, color: String, speed: String) {
-        this.name = name
-        this.weight = weight
-        this.color = color
+//    Secondary constructor
+    constructor(name: String, weight: Float, color: String, speed: String) : this(name, weight, color) {
         this.speed = speed
     }
 
-    constructor(speed: String) {
-        this.speed = speed
-    }
-
-    constructor(color: String, speed: String) {
-        this.color = color
-        this.speed = speed
+    fun doSomething(work: String): String {
+        print("Do $work")
+        return "Finish"
     }
 }
